7c468a1631d26ac580cff3ae652d69a568a2b66f
Alla Magina <babara@flylady.su>
2023-07-30 14:10:19 +0300

    fix: add default PORT

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const routes = require('./routes');
 const handleErrors = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT, DATABASE_URL } = process.env;
+const { PORT = 3000, DATABASE_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 app.use(cors({
